7834afdca39aa857e5261be5f49b921f90c107ff
Sadananda Devappa Jagadar <48876597+SadanandaJ@users.noreply.github.com>
2026-02-02 08:57:42 +0530

    Update backend.tf

--- envs/dev/backend.tf
+++ envs/dev/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket         = "terraform-state-sadananda-dev-98231"
+    bucket         = "terraform-state-sadananda02022026-dev-98231"
     key            = "dev/terraform.tfstate"
     region         = "us-east-1"
     dynamodb_table = "terraform-lock"
