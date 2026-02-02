ad3e6f202e71d6e96fa98f6b007ef9d8ecaa8a2c
Sadananda Devappa Jagadar <48876597+SadanandaJ@users.noreply.github.com>
2026-02-02 08:20:17 +0530

    Update main.tf

--- envs/dev/main.tf
+++ envs/dev/main.tf
@@ -3,13 +3,14 @@ provider "aws" {
 }
 
 module "vpc" {
-  source              = "../../modules/vpc"
-  name                = "dev-vpc"
-  vpc_cidr            = "10.0.0.0/16"
-  public_subnet_cidr  = "10.0.1.0/24"
-  private_subnet_cidr = "10.0.2.0/24"
-  az                  = "us-east-1a"
+  source               = "../../modules/vpc"
+  name                 = "dev-vpc"
+  vpc_cidr             = "10.0.0.0/16"
+  public_subnet_cidrs  = ["10.0.1.0/24", "10.0.2.0/24"]   # list
+  private_subnet_cidrs = ["10.0.101.0/24", "10.0.102.0/24"] # list
+  azs                  = ["us-east-1a", "us-east-1b"]      # list
 }
+
 
 resource "aws_security_group" "alb_sg" {
   vpc_id = module.vpc.vpc_id
@@ -33,20 +34,20 @@ module "alb" {
   source         = "../../modules/alb"
   name           = "dev-alb"
   vpc_id         = module.vpc.vpc_id
-  public_subnets = [module.vpc.public_subnet_id]
+  public_subnets = module.vpc.public_subnet_ids   # <- use plural output
   sg_id          = aws_security_group.alb_sg.id
 }
 
 module "asg" {
-  source            = "../../modules/asg"
-  ami_id            = var.ami_id
-  instance_type     = var.instance_type
-  key_name          = var.key_name
-  private_subnets   = [module.vpc.private_subnet_id]
+  source          = "../../modules/asg"
+  ami_id          = var.ami_id
+  instance_type   = var.instance_type
+  key_name        = var.key_name
+  private_subnets = module.vpc.private_subnet_ids  # <- use plural output
   target_group_arn = module.alb.target_group_arn
-  min               = var.min
-  max               = var.max
-  desired           = var.desired
-  user_data         = file("${path.module}/user_data.sh")
+  min             = var.min
+  max             = var.max
+  desired         = var.desired
+  user_data       = file("${path.module}/user_data.sh")
 }
 
